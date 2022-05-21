0c9d7e755b8a645d20d15dbb20638cd788b21cac
Igor B. Poretsky <poretsky@mlbox.ru>
2022-05-21 11:55:07 +0300

    GPL

--- youtube-dl.el
+++ youtube-dl.el
@@ -1,11 +1,29 @@
 ;;; youtube-dl.el --- manages a youtube-dl queue -*- lexical-binding: t; -*-
 
-;; This is free and unencumbered software released into the public domain.
+;; Copyright (C) 2022 Igor B. Poretsky <poretsky@mlbox.ru>
 
-;; Author: Christopher Wellons <wellons@nullprogram.com>
+;; Original author: Christopher Wellons <wellons@nullprogram.com>
 ;; URL: https://github.com/skeeto/youtube-dl-emacs
 ;; Version: 1.0
 ;; Package-Requires: ((emacs "24.3"))
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program; if not, you can either send email to this
+;; program's maintainer or write to: The Free Software Foundation,
+;; Inc.; 59 Temple Place, Suite 330; Boston, MA 02111-1307, USA.
+
 
 ;;; Commentary:
 
