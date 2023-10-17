11b0609beed4690823c98e1834af63895cdb3b8b
Igor B. Poretsky <poretsky@mlbox.ru>
2023-10-17 20:15:58 +0300

    Shortcuts transposition

--- youtube-dl-autoloads.el
+++ youtube-dl-autoloads.el
@@ -53,6 +53,6 @@
 (eval-after-load 'w3m
   '(progn
      (define-key w3m-mode-map "y" #'youtube-dl-w3m-menu-popup)
-     (define-key w3m-mode-map "Y" #'youtube-dl-w3m-invidious)
-     (define-key w3m-mode-map "i" #'youtube-dl-w3m-invidious-search)
+     (define-key w3m-mode-map "Y" #'youtube-dl-w3m-invidious-search)
+     (define-key w3m-mode-map "i" #'youtube-dl-w3m-invidious)
      (define-key w3m-mode-map "\r" #'youtube-dl-w3m-dispatch)))
