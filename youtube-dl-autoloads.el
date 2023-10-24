14068e5a973496f2dae6555249634903fb98d781
Igor B. Poretsky <poretsky@mlbox.ru>
2023-10-24 14:02:24 +0300

    Load W3m extension module together with the browser itself

--- youtube-dl-autoloads.el
+++ youtube-dl-autoloads.el
@@ -51,8 +51,4 @@
 
 ;; Key bindings for w3m:
 (eval-after-load 'w3m
-  '(progn
-     (define-key w3m-mode-map "y" #'youtube-dl-w3m-menu-popup)
-     (define-key w3m-mode-map "Y" #'youtube-dl-w3m-invidious-search)
-     (define-key w3m-mode-map "i" #'youtube-dl-w3m-invidious)
-     (define-key w3m-mode-map "\r" #'youtube-dl-w3m-dispatch)))
+  '(youtube-dl-w3m-setup))
