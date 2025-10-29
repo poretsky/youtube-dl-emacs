19478432518f4454ea0fb6ae631c007a14e4bcf9
Igor B. Poretsky <poretsky@mlbox.ru>
2025-10-29 07:47:00 +0300

    Explicit no-terminal option in the mpv invocation

--- youtube-dl-play.el
+++ youtube-dl-play.el
@@ -120,7 +120,7 @@ argument, if non-nil, is treated as start time specification string."
          (extra-options (youtube-dl-play--extra-options))
          (proc
           (apply #'start-process "mpv" nil youtube-dl-play-program
-                 "--profile=pseudo-gui" "--ytdl"
+                 "--no-terminal" "--profile=pseudo-gui" "--ytdl"
                  (nconc (list youtube-dl-play-fullscreen)
                         (when youtube-dl-play-format
                           (list (concat "--ytdl-format=" youtube-dl-play-format)))
